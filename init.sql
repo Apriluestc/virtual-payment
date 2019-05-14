1228933cef67025d33f09de0671ecd5a6f1f3187
Shiny-Man <239194147@qq.com>
2019-05-14 23:05:26 +0800

    init mysql .sql

---
+++ init.sql
@@ -0,0 +1,136 @@
+/* 创建5个表
+ * 插入初始数据
+ */
+
+
+drop table if exists txproj_buyer;
+drop table if exists txproj_seller;
+drop table if exists txproj_deal;
+drop table if exists txproj_pay;
+drop table if exists txproj_refund;
+
+
+create table if not exists txproj_buyer (
+    id          int auto_increment not null,
+    name        varchar(32) unique not null,
+    password    varchar(32) not null,
+    password_pay varchar(32) not null,
+    card        varchar(32) not null,
+    balance     bigint not null,
+    token       varchar(32),
+    primary key (id)
+)default charset=utf8 auto_increment=1000001;
+
+
+create table if not exists txproj_seller (
+    id          int auto_increment not null,
+    name        varchar(32) unique not null,
+    password    varchar(32) not null,
+    card        varchar(32) not null,
+    balance     bigint not null,
+    token       varchar(32),
+    primary key (id)
+)default charset=utf8 auto_increment=1001;
+
+
+create table if not exists txproj_deal (
+    id          int auto_increment not null,
+    buyer_id    int not null,
+    seller_id   int not null,
+    amount      bigint not null,
+    refund      bigint not null,
+    create_time datetime not null/* default CURRENT_TIMESTAMP */,
+    status      int not null,
+    /* 1：待支付
+       2：已支付
+       3：已退款
+     */
+    primary key (id)
+)default charset=utf8 auto_increment=1000000001;
+
+create table if not exists txproj_pay (
+    id          int auto_increment not null,
+    deal_id     int,
+    buyer_id    int not null,
+    seller_id   int not null,
+    amount      varchar(32) not null,
+    pay_time    datetime not null/* default CURRENT_TIMESTAMP */,
+    primary key (id)
+)default charset=utf8 auto_increment=1000000001;
+
+
+create table if not exists txproj_refund (
+    id          int auto_increment not null,
+    deal_id     int not null,
+    buyer_id    int not null,
+    seller_id   int not null,
+    amount      varchar(32) not null,
+    refund_time datetime not null/* default CURRENT_TIMESTAMP */,
+    primary key (id)
+)default charset=utf8 auto_increment=1000000001;
+
+/* insert */
+
+insert into txproj_buyer
+    (name, password, password_pay, card, balance)
+    values 
+    ("simple", "123456", "654321", "622202 4602079853882", 123456),
+    ("jinanyuan", "jinanyuan", "jinanyuan", "622202 1608089953883", 1000),
+    ("rich", "rich", "richer", "6217003320116720498", 100000000),
+    ("poor", "poor", "poorer", "6217013320216530493", 100);
+
+
+insert into txproj_seller
+    (name, password, card, balance)
+    values 
+    ("simple", "123456", "622202 4602079853882", 123456),
+    ("jinanyuan", "jinanyuan", "622202 1608089953883", 1000),
+    ("rich", "rich", "6217003320116720498", 100000000),
+    ("poor", "poor", "6217013320216530493", 100);
+
+
+insert into txproj_deal
+    (buyer_id, seller_id, amount, refund, create_time, status)
+    values
+    (1000001, 1002, 120000, 0, "2014-07-24 09:30", 2),
+    (1000003, 1002, 13300, 300, "2014-07-28 09:00", 3),     
+    (1000004, 1002, 88000, 0, "2014-08-01 10:50", 1),       
+    (1000001, 1002, 1400, 0, "2014-08-08 18:08", 1),        
+    (1000003, 1002, 3500, 0, "2014-08-08 19:01", 1),        
+    (1000004, 1002, 80100, 0, "2014-08-09 14:25", 2),       
+    (1000001, 1003, 1400, 0, "2014-08-09 14:30", 2),        
+    (1000002, 1001, 38300, 0, "2014-08-10 19:30", 1),       
+    (1000001, 1003, 1800, 100, "2014-08-11 09:00", 3),      
+    (1000003, 1002, 3700, 0, "2014-08-12 09:20", 1),        
+    (1000003, 1004, 71300, 0, "2014-08-13 19:30", 2),       
+    (1000003, 1004, 12300, 0, "2014-08-14 10:30", 1);       
+    (1000011, 1012, 220000, 0, "2015-07-24 09:30", 2),
+    (1000013, 1012, 23300, 300, "2015-07-28 09:00", 3),     
+    (1000014, 1012, 28000, 0, "2015-08-01 10:50", 1),       
+    (1000011, 1012, 2400, 0, "2015-08-08 18:08", 1),        
+    (1000013, 1012, 2500, 0, "2015-08-08 19:01", 1),        
+    (1000014, 1012, 20100, 0, "2015-08-09 14:25", 2),       
+    (1000011, 1013, 2400, 0, "2015-08-09 14:30", 2),        
+    (1000012, 1011, 28300, 0, "2015-08-10 19:30", 1),       
+    (1000011, 1013, 2800, 100, "2015-08-11 09:00", 3),      
+    (1000013, 1012, 2700, 0, "2015-08-12 09:20", 1),        
+    (1000013, 1014, 21300, 0, "2015-08-13 19:30", 2),       
+    (1000013, 1014, 22300, 0, "2015-08-14 10:30", 1);       
+
+
+insert into txproj_pay
+    (deal_id, buyer_id, seller_id, amount, pay_time)
+    values
+    (1000000001, 1000001, 1002, 120000, "2014-07-24 09:35"),
+    (1000000002, 1000003, 1002, 13300, "2014-07-28 09:10"),
+    (1000000006, 1000004, 1002, 3400, "2014-08-09 19:10"),
+    (1000000007, 1000001, 1003, 1400, "2014-08-09 19:40"),
+    (1000000009, 1000001, 1003, 1800, "2014-08-11 11:10"),
+    (1000000011, 1000003, 1004, 71300, "2014-08-13 21:10");
+
+
+insert into txproj_refund
+    (deal_id, buyer_id, seller_id, amount, refund_time)
+    values
+    (1000000002, 1000003, 1002, 300, "2014-07-29 12:00"),
+    (1000000009, 1000001, 1003, 100, "2014-08-11 11:10");
